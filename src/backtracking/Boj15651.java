4ccfe01717bcd6ed6dc2049815065f577cd2b98b
jaeryong <frogs6225@naver.com>
2024-07-01 19:15:23 +0900

    feat : 20240701

---
+++ src/backtracking/Boj15651.java
@@ -0,0 +1,38 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj15651 {
+    static int N;
+    static int M;
+    static int[] ch;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        ch = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int L) {
+        if (L == M) {
+            for(int a : ch) { sb.append(a).append(" ");}
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            ch[L] = i+1;
+            dfs(L+1);
+        }
+
+    }
+
+}
+
