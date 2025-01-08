5de998dccfbd5873a7b72eb6a1e3e7b035ef3520
jaeryong <frogs6225@naver.com>
2025-01-08 22:09:17 +0900

    feat : 20250108

---
+++ src/arithmetic_operation/Boj10172.java
@@ -0,0 +1,13 @@
+package arithmetic_operation;
+
+import java.io.IOException;
+
+public class Boj10172 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
---
+++ src/arithmetic_operation/Boj11382.java
@@ -0,0 +1,13 @@
+package arithmetic_operation;
+
+import java.io.IOException;
+
+public class Boj11382 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
