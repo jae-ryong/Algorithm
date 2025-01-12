90d0f0a14859988a0328256004db5181c96d2672
jaeryong <frogs6225@naver.com>
2025-01-12 15:55:03 +0900

    feat : 20250112

---
+++ src/loop/Boj10951.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String numbers;
+        while((numbers = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(numbers);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A+B);
+        }
+    }
+}
---
+++ src/loop/Boj2438.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for(int j=0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
